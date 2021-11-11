a52de2f2d32ad7ddbaadde283452877a30742f80
Mahdieh <mahdeh.abed@gmail.com>
2021-11-11 19:25:47 +0330

    100 done

--- project/JS/project-4.js
+++ project/JS/project-4.js
@@ -9,14 +9,14 @@ function Card(image, name, rate, updateDate, runningTime) {
 const cardsInfo = [];
 
 // movie entry
-cardsInfo.push(new Card('../Img/ََAvengersEndgame.jpg', 'ََAvengers: Endgame', 8.8, `SUN 8 SEPT - 10:00PM`, `2HR 05MIN`))
-cardsInfo.push(new Card(`../Img/Hobbs&Shaw.jpg`, `Hobbs & Shaw`, 9.7, `TUE 4 JULY - 05:00PM`, `2HR 13MIN`))
-cardsInfo.push(new Card(`../Img/JohnWick3.jpg`, `John Wick 3`, 9.8, `WED 16 AUG - 07:00PM`, `2HR 33 MIN`))
-cardsInfo.push(new Card(`../Img/Deadpool2.jpg`, `Deadpool 2`, 7.3, `MON 16 OCT - 10:00PM`, `2HR 10MIN`))
-cardsInfo.push(new Card(`../Img//TheLionKing.jpg`, `The Lion King`, 8.1, `SUN 8 SEPT - 10:00PM`, `2HR 38MIN`))
-cardsInfo.push(new Card(`../Img//MadMaxFuryRoad.jpg`, `Mad Max: Fury Road`, 7.4, `TUE 8 JULY - 05:00PM`, `2HR 11MIN`))
-cardsInfo.push(new Card(`../Img//AquaMan.jpg`, `AquaMan`, 8.1, `WED 16 AUG - 07:00PM`, `2HR 14MIN`))
-cardsInfo.push(new Card(`../Img//MissionImpossible.jpg`, `Mission Impossible`, 7.8, `MON 18 OCT - 10:00PM`, `2HR 32MIN`))
+cardsInfo.push(new Card('../Img/AvengersEndgame.jpg', `ََAVENGER ENDGAME`, `8.8`, `SUN 8 SEPT - 10:00PM`, `2HR 05MIN`))
+cardsInfo.push(new Card(`../Img/Hobbs&Shaw.jpg`, `HOBBS & SHAW`, `9.7`, `TUE 4 JULY - 05:00PM`, `2HR 13MIN`))
+cardsInfo.push(new Card(`../Img/JohnWick3.jpg`, `JOHN WICK 3`, `9.8`, `WED 16 AUG - 07:00PM`, `2HR 33 MIN`))
+cardsInfo.push(new Card(`../Img/Deadpool2.jpg`, `DEADPOOL 2`, `7.3`, `MON 16 OCT - 10:00PM`, `2HR 10MIN`))
+cardsInfo.push(new Card(`../Img//TheLionKing.jpg`, `THE LION KING`, `8.1`, `SUN 8 SEPT - 10:00PM`, `2HR 38MIN`))
+cardsInfo.push(new Card(`../Img//MadMaxFuryRoad.jpg`, `MAD MAX FURY ROAD`, `7.4`, `TUE 8 JULY - 05:00PM`, `2HR 11MIN`))
+cardsInfo.push(new Card(`../Img//AquaMan.jpg`, `AQUA MAN`, `8.1`, `WED 16 AUG - 07:00PM`, `2HR 14MIN`))
+cardsInfo.push(new Card(`../Img//MissionImpossible.jpg`, `MISSION IMPOSSIBLE`, `7.8`, `MON 18 OCT - 10:00PM`, `2HR 32MIN`))
 
 const cardView = (cardInfo) => {
     return `
@@ -53,11 +53,12 @@ const cardContainer = document.getElementById("main__content")
 cardContainer.innerHTML = generateCards(cardsInfo)
 
 function onSearch(element) {
-    const searchTerm = element.value;
+    const searchTerm = element.value.toString().toUpperCase();
     const filteredCards = cardsInfo.filter(card => {
-        return card.name.includes(searchTerm) || card.rate.includes(searchTerm) || card.runningTime.includes(searchTerm) || card.updateDate.includes(searchTerm)
+        return card.name.includes(searchTerm) || card.rate.includes(searchTerm) || card.rate.includes(searchTerm) || card.updateDate.includes(searchTerm)
     })
+
     cardContainer.innerHTML = generateCards(filteredCards)
 }
 
-onSearch(1917)
+
